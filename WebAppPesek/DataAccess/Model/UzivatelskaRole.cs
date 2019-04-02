87419a2111c147dfa9517518953910156980fea5
Viktor Pešek <viktor.pesek12@gmail.com>
2019-04-02 18:26:28 +0200

    add uzivatelska role class

--- WebAppPesek/DataAccess/Model/UzivatelskaRole.cs
+++ WebAppPesek/DataAccess/Model/UzivatelskaRole.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DataAccess.Interface;
 
 namespace DataAccess.Model
 {
-    public class UzivatelskaRole
+    public class UzivatelskaRole : IEntity
     {
+        public virtual int Id { get; set; }
+
+        public virtual string Nazev { get; set; }
+
+        public virtual string Popis { get; set; }
     }
 }
