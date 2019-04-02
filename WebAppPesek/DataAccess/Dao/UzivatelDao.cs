bedfe6f4fc4915ec0fac8d6da732a8d8e2ba90fb
Viktor Pešek <viktor.pesek12@gmail.com>
2019-04-02 18:25:08 +0200

    Add uzivatelska role and mappings with dao

---
+++ WebAppPesek/DataAccess/Dao/UzivatelDao.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess.Model;
+using NHibernate.Criterion;
+
+namespace DataAccess.Dao
+{
+    class UzivatelDao : DaoBase<Uzivatel>
+    {
+        public Uzivatel getByLoginAndPassword(string login, string password)
+        {
+            return session.CreateCriteria<Uzivatel>()
+                .Add(Restrictions.Eq("Login", login))
+                .Add(Restrictions.Eq("Heslo", password))
+                .UniqueResult<Uzivatel>();
+        }
+
+        public Uzivatel getByLogin(string login)
+        {
+            return session.CreateCriteria<Uzivatel>()
+                .Add(Restrictions.Eq("Login", login))
+                .UniqueResult<Uzivatel>();
+        }
+    }
+}
---
+++ WebAppPesek/DataAccess/Mappings/UzivatelskaRoleMap.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess.Model;
+using FluentNHibernate.Mapping;
+
+namespace DataAccess.Mappings
+{
+    class UzivatelskaRoleMap : ClassMap<UzivatelskaRole>
+    {
+        public UzivatelskaRoleMap()
+        {
+            Table("role");
+            Id(x => x.Id).Column("role_id").GeneratedBy.Native().Not.Nullable();
+            Map(x => x.Popis).Column("popis").Not.Nullable();
+            Map(x => x.Nazev).Column("nazev").Not.Nullable();
+        }
+    }
+}
