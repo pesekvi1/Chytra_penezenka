244a89f358c286c0cc72c3a4897d03cca03be317
Viktor Pešek <viktor.pesek12@gmail.com>
2019-04-08 15:48:45 +0200

    add skupina model class

---
+++ WebAppPesek/DataAccess/Model/Skupina.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess.Interface;
+
+namespace DataAccess.Model
+{
+    public class Skupina : IEntity
+    {
+        public virtual int Id { get; set; }
+        public virtual Uzivatel Zakladatel { get; set; }
+        public virtual string Nazev { get; set; }
+        public virtual IList<Uzivatel> Uzivatele { get; set; }
+
+    }
+}
