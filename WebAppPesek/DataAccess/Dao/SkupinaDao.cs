993044d552d05254a60303db026b18d8bb5f0234
Viktor Pešek <viktor.pesek12@gmail.com>
2019-04-08 15:44:29 +0200

    Add dao classes

---
+++ WebAppPesek/DataAccess/Dao/SkupinaDao.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess.Model;
+using NHibernate.Criterion;
+
+namespace DataAccess.Dao
+{
+    public class SkupinaDao : DaoBase<Skupina>
+    {
+        public IList<Skupina> getMyGroups(Uzivatel Zakladatel)
+        {
+            return session.CreateCriteria<Skupina>().Add(Restrictions.Eq("Zakladatel", Zakladatel)).List<Skupina>();
+        }
+    }
+}
