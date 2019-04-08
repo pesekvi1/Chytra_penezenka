362e5ece728bf3e2cd501786b292b4c0298c1183
Viktor Pešek <viktor.pesek12@gmail.com>
2019-04-08 15:49:59 +0200

    refactor homeController to show curr user

--- WebAppPesek/WebAppPesek/Controllers/HomeController.cs
+++ WebAppPesek/WebAppPesek/Controllers/HomeController.cs
@@ -3,14 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using DataAccess.Dao;
+using DataAccess.Model;
 
 namespace WebAppPesek.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult Uzivatel()
+        {
+            UzivatelDao uzivatelDao = new UzivatelDao();
+            Uzivatel uzivatel = uzivatelDao.getByLogin(User.Identity.Name);
+
+            return View(uzivatel);
         }
 
         public ActionResult About()
