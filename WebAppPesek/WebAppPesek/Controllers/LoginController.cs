b16724cc7da24d1fc2827bc1d1401da69654e46a
Viktor Pešek <viktor.pesek12@gmail.com>
2019-04-08 15:47:34 +0200

    Adding new controllers

---
+++ WebAppPesek/WebAppPesek/Controllers/LoginController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace WebAppPesek.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SignIn(string login, string password)
+        {
+            if (Membership.ValidateUser(login, password))
+            {
+                FormsAuthentication.SetAuthCookie(login, false);
+                Session["user_id"] = login;
+                return RedirectToAction("Index", "Home");
+            }
+
+            TempData["error-message"] = "Login nebo heslo neni spravne";
+            return RedirectToAction("Index", "Login");
+        }
+
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            Session.Clear();
+
+            return RedirectToAction("Index", "Login");
+        }
+    }
+}
---
+++ WebAppPesek/WebAppPesek/Controllers/SkupinaController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DataAccess.Dao;
+using DataAccess.Model;
+
+namespace WebAppPesek.Controllers
+{
+    [Authorize]
+    public class SkupinaController : Controller
+    {
+        // GET: Skupina
+        public ActionResult Index()
+        {
+            UzivatelDao uzivatelDao = new UzivatelDao();
+            Uzivatel uzivatel = uzivatelDao.getByLogin(User.Identity.Name);
+            SkupinaDao skupinaDao = new SkupinaDao();
+            //IList<Skupina> skupiny = skupinaDao.getMyGroups(uzivatel);
+            IList<Skupina> skupiny = skupinaDao.getMyGroups(uzivatel);
+            ViewBag.ListJePrazdny = false;
+
+            if (skupiny.Count == 0)
+            {
+                ViewBag.ListJePrazdny = true;
+            }
+
+            ViewBag.MeSkupiny = skupiny;
+
+            return View();
+        }
+
+        public ActionResult Detail(int id)
+        {
+            SkupinaDao skupinaDao = new SkupinaDao();
+            Skupina skupina = skupinaDao.GetById(id);
+            ViewBag.Uzivatele = skupina.Uzivatele;
+            return View();
+        }
+
+        public ActionResult PridaniSkupiny(Skupina skupina)
+        {
+            UzivatelDao uzivatelDao = new UzivatelDao();
+            Uzivatel uzivatel = uzivatelDao.getByLogin(User.Identity.Name);
+
+            skupina.Zakladatel = uzivatel;
+            
+            if (ModelState.IsValid)
+            {
+                SkupinaDao skupinaDao = new SkupinaDao();
+                skupinaDao.Create(skupina);
+                TempData["message-success"] = "Skupina byla uspesne pridana";
+
+                return RedirectToAction("Index", "Skupina");
+            }
+            else
+            {
+                return View("VytvoreniSkupiny", skupina);
+            }
+        }
+
+        
+        public ActionResult VytvoreniSkupiny()
+        {
+            return View();
+        }
+    }
+}
