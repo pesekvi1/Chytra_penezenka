d68d027b03f2367bb0e92ef87fa0fddcaacc72a1
Viktor Pešek <viktor.pesek12@gmail.com>
2019-04-09 18:40:00 +0200

    Refactoring mappings

--- WebAppPesek/DataAccess/Mappings/UzivatelMap.cs
+++ WebAppPesek/DataAccess/Mappings/UzivatelMap.cs
@@ -21,7 +21,8 @@ namespace DataAccess.Mappings
             Map(x => x.Login).Column("login");
             Map(x => x.Heslo).Column("heslo");
             References(x => x.Role).ForeignKey("FK_uzivatel_uzivatel_role_id").Column("role_id");
-            References(x => x.Skupina).ForeignKey("FK_uzivatel_skupina_id").Nullable();
+            References(x => x.Skupina).ForeignKey("FK_uzivatel_skupina_id").Nullable().Cascade.SaveUpdate();
+            References(x => x.Vytvoril).ForeignKey("FK_uzivatel_uzivatel_id").Nullable();
         }
     }
 }
