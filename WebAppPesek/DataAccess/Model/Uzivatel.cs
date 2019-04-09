17a8c0f85fa40671e9d7dba085f6ba30b528ac40
Viktor Pešek <viktor.pesek12@gmail.com>
2019-04-09 18:39:15 +0200

    Basic validation of user

--- WebAppPesek/DataAccess/Model/Uzivatel.cs
+++ WebAppPesek/DataAccess/Model/Uzivatel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,16 +13,22 @@ namespace DataAccess.Model
     {
         public virtual int Id { get; set; }
 
+        [Required]
         public virtual string Jmeno { get; set; }
 
+        [Required]
         public virtual string Prijmeni { get; set; }
 
+        [Required]
         public virtual string Login { get; set; }
 
+        [Required]
         public virtual string Heslo { get; set; }
 
         public virtual UzivatelskaRole Role { get; set; }
 
         public virtual Skupina Skupina { get; set; }
+
+        public virtual Uzivatel Vytvoril { get; set; }
 }
 }
