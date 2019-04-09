6dec719ef2a9e26b344e80fd243f7cd186cfc407
Viktor Pešek <viktor.pesek12@gmail.com>
2019-04-09 18:38:41 +0200

    adding editable group to controller

--- WebAppPesek/WebAppPesek/Controllers/SkupinaController.cs
+++ WebAppPesek/WebAppPesek/Controllers/SkupinaController.cs
@@ -60,6 +60,55 @@ namespace WebAppPesek.Controllers
             }
         }
 
+        public ActionResult Editace(int id)
+        {
+            SkupinaDao skupinaDao = new SkupinaDao();
+            Skupina skupina = skupinaDao.GetById(id);
+            IList<Uzivatel> uzivatele = skupina.Uzivatele;
+            ViewBag.Uzivatele = uzivatele;
+
+            return View(skupina);
+        }
+
+        public ActionResult UpraveniSkupiny(Skupina skupina)
+        {
+            try
+            {
+                SkupinaDao skupinaDao = new SkupinaDao();
+
+                Skupina staraSkupina = skupinaDao.GetById(skupina.Id);
+                staraSkupina.Nazev = skupina.Nazev;
+
+
+                skupinaDao.Update(staraSkupina);
+                TempData["message-success"] = "Skupina " + skupina.Nazev + " byla úspěšně upravena";
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+
+            return RedirectToAction("Index", "Skupina");
+        }
+
+        public ActionResult Odstranit(int id)
+        {
+            try
+            {
+                SkupinaDao skupinaDao = new SkupinaDao();
+                Skupina skupina = skupinaDao.GetById(id);
+                skupinaDao.Delete(skupina);
+                TempData["message-success"] = "Skupina " + skupina.Nazev + " byla odstraněna";
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+
+            return RedirectToAction("Index", "Skupina");
+        }
         
         public ActionResult VytvoreniSkupiny()
         {
