98373673ecdd2b4734c90da59f2e2ad5fb50f2ff
Viktor Pešek <viktor.pesek12@gmail.com>
2019-04-13 15:33:15 +0200

    refactoring

--- WebAppPesek/WebAppPesek/Controllers/UzivatelController.cs
+++ WebAppPesek/WebAppPesek/Controllers/UzivatelController.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Helpers;
 using System.Web.Mvc;
 using DataAccess.Dao;
 using DataAccess.Model;
@@ -15,17 +16,17 @@ namespace WebAppPesek.Controllers
         public ActionResult Index()
         {
             UzivatelDao uzivatelDao = new UzivatelDao();
-            Uzivatel uzivatel = uzivatelDao.getByLogin(User.Identity.Name);
+            Uzivatel uzivatel = uzivatelDao.GetByLogin(User.Identity.Name);
             IList<Uzivatel> uzivatele = uzivatelDao.GetUsersForAdmin(uzivatel);
             ViewBag.Uzivatele = uzivatele;
             return View();
         }
 
-        public ActionResult PridatUzivatee()
+        public ActionResult PridatUzivatele()
         {
             SkupinaDao skupinaDao = new SkupinaDao();
             UzivatelDao uzivatelDao = new UzivatelDao();
-            Uzivatel uzivatel = uzivatelDao.getByLogin(User.Identity.Name);
+            Uzivatel uzivatel = uzivatelDao.GetByLogin(User.Identity.Name);
             IList<Skupina> skupiny = skupinaDao.getMyGroups(uzivatel);
             ViewBag.Skupiny = skupiny;
 
@@ -37,26 +38,26 @@ namespace WebAppPesek.Controllers
         }
 
         [HttpPost]
-        public ActionResult Vytvorit(Uzivatel uzivatel, int skupinaID, int roleID)
+        public ActionResult Vytvorit(Uzivatel uzivatel, int skupinaId)
         {
 
             SkupinaDao skupinaDao = new SkupinaDao();
-            Skupina skupina = skupinaDao.GetById(skupinaID);
+            Skupina skupina = skupinaDao.GetById(skupinaId);
             skupinaDao.CloseSession();
 
             UzivatelskaRoleDao uzivatelskaRoleDao = new UzivatelskaRoleDao();
-            UzivatelskaRole role = uzivatelskaRoleDao.GetById(roleID);
+            UzivatelskaRole role = uzivatelskaRoleDao.getRoleWithName("uzivatel");
             uzivatelskaRoleDao.CloseSession();
 
             UzivatelDao uzivatelDao = new UzivatelDao();
-            Uzivatel admin = uzivatelDao.getByLogin(User.Identity.Name);
+            Uzivatel admin = uzivatelDao.GetByLogin(User.Identity.Name);
             uzivatel.Skupina = skupina;
             uzivatel.Vytvoril = admin;
             uzivatel.Role = role;
 
             if (ModelState.IsValid)
             {
-                uzivatelDao.Create(uzivatel);
+                uzivatelDao.CreateWithHashedPassword(uzivatel);
                 TempData["message-success"] = "Uživatel úspěšne přidán";
             }
 
