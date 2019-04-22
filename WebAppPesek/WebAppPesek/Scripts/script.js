299d66e5b375e272a5d911e71483c99117ecf604
Viktor Pešek <viktor.pesek12@gmail.com>
2019-04-22 22:33:29 +0200

    Adding scripts files for datepicker

---
+++ WebAppPesek/WebAppPesek/Scripts/script.js
@@ -0,0 +1,35 @@
+﻿$(document).ready(function () {
+    loaddatepicker();
+});
+
+$(document).ajaxComplete(function () {
+    loaddatepicker();
+});
+
+function loaddatepicker() {
+
+    //Only if it's a non touch device.
+    if (!Modernizr.touch) {
+        if (Modernizr.inputtypes.date) {
+
+            //Disable default html5 datepicker
+            $('input[type="date"]').on("click", function (event) {
+                event.preventDefault();
+            });
+
+            //Enable jquery-ui date time picker
+            $('input[type="date"]').datepicker({
+                dateFormat: "yy-mm-dd"
+            });
+
+        } else {
+            if (!Modernizr.inputtypes.date) {
+                //This happens if device is touch and 
+                //does not support html5 date time
+                $('input[type="date"]').datepicker({
+                    dateFormat: "yy-mm-dd"
+                });
+            }
+        }
+    }
+}
