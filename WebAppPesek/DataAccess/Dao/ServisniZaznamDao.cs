dd95707f6f1d70d779ed72bcc673fdd1e754d2f0
Viktor Pešek <viktor.pesek12@gmail.com>
2019-04-27 19:48:17 +0200

    New dao classes and refactoring old

---
+++ WebAppPesek/DataAccess/Dao/ServisniZaznamDao.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess.Model;
+using NHibernate.Criterion;
+
+namespace DataAccess.Dao
+{
+    public class ServisniZaznamDao : DaoBase<ServisniZaznam>
+    {
+        public IList<ServisniZaznam> GetZaznamyForVozidloPaged(Vozidlo vozidlo, int count, int page, out int totalItems)
+        {
+            totalItems = session.CreateCriteria<ServisniZaznam>()
+                .Add(Restrictions.Eq("Vozidlo", vozidlo))
+                .SetProjection(Projections.RowCount()).UniqueResult<int>();
+
+            return session.CreateCriteria<ServisniZaznam>()
+                .Add(Restrictions.Eq("Vozidlo", vozidlo))
+                .SetFirstResult((page - 1) * count).SetMaxResults(count)
+                .List<ServisniZaznam>();
+        }
+    }
+}
--- WebAppPesek/DataAccess/Dao/VozidloDao.cs
+++ WebAppPesek/DataAccess/Dao/VozidloDao.cs
@@ -4,10 +4,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using DataAccess.Model;
+using NHibernate.Criterion;
 
 namespace DataAccess.Dao
 {
     public class VozidloDao : DaoBase<Vozidlo>
     {
+        public IList<Vozidlo> GetVozidlaPaged(Uzivatel vlastnik, int count, int page, out int totalItems)
+        {
+            totalItems = session.CreateCriteria<Vozidlo>()
+                .Add(Restrictions.Eq("Vlastnik", vlastnik))
+                .SetProjection(Projections.RowCount()).UniqueResult<int>();
+
+            return session.CreateCriteria<Vozidlo>()
+                .Add(Restrictions.Eq("Vlastnik", vlastnik))
+                .SetFirstResult((page - 1) * count).SetMaxResults(count)
+                .List<Vozidlo>();
+        }
+        
     }
 }
